d7acbad999e36211ed9aa3ddc16f751010b6d28a
Omi0077 <omshankar2016@gmail.com>
2026-06-04 23:36:55 +0530

    starting to work on how to make this work with execv, by taking path from user

--- main.c
+++ main.c
@@ -5,33 +5,109 @@
 #include <fcntl.h>
 #include <sys/wait.h>
 
-typedef struct{
-  char** buf;
+typedef struct
+{
+  char **buf;
   int size;
 } parseLineRet;
 
+char* PATH = "/bin";
 
-parseLineRet* parseLine(char* line, char* delimiter){
-  char** buf;
+void errorOccured();
+parseLineRet *parseLine(char *line, char *delimiter);
+void runCommand(int argc, char *argv[]);
+void addPath(int argc, char *argv[]);
+
+
+int main(int argc, char *argv[])
+{
+  if (argc > 1)
+  {
+    fprintf(stdout, "%s %s\n", argv[0], argv[1]);
+  }
+  else
+  {
+    while (1)
+    {
+      fprintf(stdout, "wish> ");
+      char *line = NULL;
+      size_t len = 0;
+      getline(&line, &len, stdin);
+
+      char *delimiter = " ";
+      char **tokens;
+
+      line[strcspn(line, "\n")] = '\0';
+      // replace \n caught by getline
+
+      parseLineRet *temp = parseLine(line, delimiter);
+      tokens = temp->buf;
+      int bufsize = temp->size;
+
+      if (strcmp(tokens[0], "bye") == 0)
+      { // not checking EOF
+        free(line);
+        free(tokens);
+        exit(0);
+      }
+      else if (strcmp(tokens[0], "path") == 0)
+      {
+        //
+      }
+      else
+      {
+        char *myArgs[bufsize + 1];
+        for (int i = 0; i < bufsize; i++)
+        {
+          myArgs[i] = tokens[i];
+        }
+        myArgs[bufsize] = NULL; // set last as NULL
+
+        runCommand(bufsize, myArgs);
+      }
+
+      free(temp);
+    }
+  }
+}
+
+
+
+
+
+
+void errorOccured()
+{
+  char error_message[30] = "An error has occurred\n";
+  write(STDERR_FILENO, error_message, strlen(error_message));
+}
+
+parseLineRet *parseLine(char *line, char *delimiter)
+{
+  char **buf;
   int bufsize = 4;
   int offset = 0;
-  char* token;
+  char *token;
 
-  buf = malloc(bufsize*sizeof(char*));
-  if(buf == NULL){
+  buf = malloc(bufsize * sizeof(char *));
+  if (buf == NULL)
+  {
     return NULL;
   }
 
-  while(token = strsep(&line, delimiter)){
-    //null-terminate token
-    // printf("%s is %d long\n", token, strlen(token));
-    // already null terminated
+  while (token = strsep(&line, delimiter))
+  {
+    // null-terminate token
+    //  printf("%s is %d long\n", token, strlen(token));
+    //  already null terminated
 
     // check if buffer is full
-    if(offset == bufsize -1){
+    if (offset == bufsize - 1)
+    {
       bufsize *= 2;
-      char** new_buf = realloc(buf, bufsize*sizeof(char*));
-      if(new_buf == NULL){
+      char **new_buf = realloc(buf, bufsize * sizeof(char *));
+      if (new_buf == NULL)
+      {
         free(buf);
         return NULL;
       }
@@ -42,9 +118,11 @@ parseLineRet* parseLine(char* line, char* delimiter){
   }
 
   // shrinking buffer to fit
-  if(offset < bufsize-1){
-    char** new_buf = realloc(buf, (offset+1)*sizeof(char*));
-    if(new_buf != NULL){
+  if (offset < bufsize - 1)
+  {
+    char **new_buf = realloc(buf, (offset + 1) * sizeof(char *));
+    if (new_buf != NULL)
+    {
       buf = new_buf;
     }
   }
@@ -55,64 +133,45 @@ parseLineRet* parseLine(char* line, char* delimiter){
   return temp;
 }
 
-void runCommand(int argc, char* argv[]){
-  if(argc < 1){
+void runCommand(int argc, char *argv[])
+{
+  if (argc < 1)
+  {
     exit(EXIT_FAILURE);
   }
   int pid = fork();
-  if(pid == -1){
+  if (pid == -1)
+  {
     perror("fork");
     exit(EXIT_FAILURE);
   }
-  else if(pid == 0){
-    //child process
+  else if (pid == 0)
+  {
+    // child process
+    // check if executable exists
+    int executableExist = access(argv[0], X_OK);
+    if (executableExist == -1)
+    {
+      errorOccured();
+      perror("executable not found");
+      exit(EXIT_FAILURE);
+    }
     execvp(argv[0], argv);
     printf("this shouldnt print if everythings fine\n");
     exit(EXIT_FAILURE);
   }
-  else{
+  else
+  {
     int terminatedPid = waitpid(pid, NULL, 0);
     // wait(NULL);
   }
 }
 
-int main(int argc, char* argv[]){
-  if(argc > 1){
-    fprintf(stdout, "%s %s\n", argv[0], argv[1]);
+void addPath(int argc, char *argv[]){
+  if(argc == 0){
+    PATH = NULL;
   }
-  else{
-    while(1){
-      fprintf(stdout, "wish> ");
-      char* line = NULL;
-      size_t len = 0;
-      getline(&line, &len, stdin);
-
-      char* delimiter = " ";
-      char** tokens;
-
-      if(strcmp(line,"bye\n") == 0){ // not checking EOF
-        free(line);
-        free(tokens);
-        exit(0);
-      } 
-
-      line[strcspn(line, "\n")] = '\0';
-      // replace \n caught by getline
-
-      parseLineRet* temp = parseLine(line, delimiter);
-      tokens = temp->buf;
-      int bufsize = temp->size;
-
-      char* args[bufsize+1];
-      for(int i=0; i<bufsize; i++){
-        args[i] = tokens[i];
-      }
-      args[bufsize] = NULL;
-      // args[0] = "ls";
-      // args[1] = NULL;
-      runCommand(bufsize,args);
-
-      free(temp);
-    }
+  else if(argc > 0){
+    
   }
 }
